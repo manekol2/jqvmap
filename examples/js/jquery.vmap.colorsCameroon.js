f76ac740ca69e8ab79fd6a93102d3884adf1cce1
manekol <manekol22@gmail.com>
2015-11-20 14:07:31 +0100

    jquery.vmap.colorsCameroon.js
    Designing Cameroon Map Vector

---
+++ examples/js/jquery.vmap.colorsCameroon.js
@@ -0,0 +1,7 @@
+/**
+	Cameroon Map Volor
+	Created By NTO'O ANGO ANDRE
+	E-mail: manekol22@gmail.com
+**/
+
+var couleurs = {"1":"#007f00","2":"#00bf00","3":"#d4ffaa","4":"#aaff56","5":"#aaff56","6":"#d4ffaa","7":"#aaff56","8":"#f9f754","9":"#aaffd4","10":"yellow"};
